e20267536bd4356dfa4d75e054b046d8515d7ad0
monikaili <iliasova.m@gmail.com>
2023-01-28 19:09:17 +0100

    first commit
    first commit

--- InfoCardStoryboard/ViewController.swift
+++ InfoCardStoryboard/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var positionLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
+       
+        
+      makeCircle()
+    
+        nameLabel.text = "Mon Ili"
+        positionLabel.text = "Junior iOS dev"
+        
+        
     }
-
-
+  
+        
+    
+  
+    
+    func makeCircle() {
+        let circle = image
+        
+        circle?.layer.cornerRadius = view.layer.bounds.width / 4
+        circle?.clipsToBounds = true
+        circle?.layer.borderColor = UIColor.white.cgColor
+        circle?.layer.borderWidth = 2.0
+        
+        
+    }
+    
+    
+    @IBAction func emailButton(_ sender: UIButton) {
+        
+        sender.setTitle("mon@ili.com", for: .normal)
+    
+    }
+    
+    @IBAction func phoneNumber(_ sender: UIButton) {
+        
+        sender.setTitle("+123 456 789", for: .normal)
+    }
+    
 }
 
